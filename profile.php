9b72553a8b8b475f85e332f454b2378a9d001a49
hailm <hai.lemanh.1310@gmail.com>
2022-10-21 18:48:22 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 <?php require "template-parts/layouts/header.php"; ?>
 <div class="uk-section-xsmall" uk-height-viewport="expand: true">
     <div class="uk-container uk-container-expand">
-        <a href="" uk-icon="icon: sign-out">Logout</a>
+        <a href="login.php" uk-icon="icon: sign-out">Logout</a>
     </div>
 </div>
 <?php require "template-parts/layouts/footer.php"; ?>
